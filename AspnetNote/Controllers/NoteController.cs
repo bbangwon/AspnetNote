aeff6afe265e288ceb8bac86b8aeebabb88b8676
breadone <bbangwon.chung@gamil.com>
2018-05-19 19:52:32 +0900

    게시판 추가

--- AspnetNote/Controllers/NoteController.cs
+++ AspnetNote/Controllers/NoteController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspnetNote.DataContext;
 using AspnetNote.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspnetNote.Controllers
@@ -16,6 +17,12 @@ namespace AspnetNote.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
+            if(HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+
             using (var db = new AspnetNoteDbContext())
             {
                 var list = db.Notes.ToList();
@@ -29,7 +36,40 @@ namespace AspnetNote.Controllers
         /// <returns></returns>
         public IActionResult Add()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Add(Note model)
+        {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+
+
+            model.UserNo = int.Parse(HttpContext.Session.GetInt32("USER_LOGIN_KEY").ToString());
+            if (ModelState.IsValid)
+            {
+                using (var db = new AspnetNoteDbContext())
+                {
+                    db.Notes.Add(model);
+
+                    if(db.SaveChanges() > 0)
+                    {
+                        return Redirect("Index");
+                    }                    
+                }
+                ModelState.AddModelError(string.Empty, "게시물을 저장할 수 없습니다.");
+            }
+            return View(model);
         }
 
         /// <summary>
@@ -38,6 +78,12 @@ namespace AspnetNote.Controllers
         /// <returns></returns>
         public IActionResult Edit()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+
             return View();
         }
 
@@ -47,6 +93,12 @@ namespace AspnetNote.Controllers
         /// <returns></returns>
         public IActionResult Delete()
         {
+            if (HttpContext.Session.GetInt32("USER_LOGIN_KEY") == null)
+            {
+                //로그인이 안된 상태
+                return RedirectToAction("Login", "Account");
+            }
+
             return View();
         }
     }
