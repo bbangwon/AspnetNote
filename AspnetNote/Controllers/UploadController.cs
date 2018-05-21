56fd80a2a23651ba20998d6e5b7a3222219d70fd
breadone <bbangwon.chung@gamil.com>
2018-05-21 21:44:32 +0900

    이미지 업로드 기능 추가

---
+++ AspnetNote/Controllers/UploadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetNote.Controllers
+{
+    public class UploadController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public UploadController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpPost, Route("api/upload")]
+        public async Task<IActionResult> ImageUpload(IFormFile file)
+        {
+            // 이미지나 파일을 업로드 할 때 필요한 구성
+            // 1. Path(경로) - 어디에 저장할지 결정
+
+            var path = Path.Combine(_environment.WebRootPath, @"images\upload");
+            // 2. Name(이름) - DateTime, GUID
+
+            var fileFullName = file.FileName.Split('.');
+            var fileName = $"{Guid.NewGuid()}.{fileFullName[1]}";
+            
+            // 3. Extension(확장자) - jpg, png... txt
+
+            using (var fileStream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+
+            return Ok(new { file="/images/upload/" + fileName, success=true});
+
+            // URL 접근방식
+            // ASP.NET - 호스트명/ + api/upload
+            // Javascript 호스트명 + api/upload
+        }
+    }
+}
--- AspnetNote/wwwroot/js/site.js
+++ AspnetNote/wwwroot/js/site.js
@@ -1,4 +1,23 @@
 ﻿// Write your JavaScript code.
 $(".editor").trumbowyg({
-    lang: 'ko'
+    lang: 'ko',
+    btnsDef: {
+        image: {
+            dropdown: ['insertImage', 'upload'],
+            ico: 'insertImage'
+        }
+    },
+    btns: [
+        ['viewHTML'],
+        ['formatting'],
+        ['strong', 'em', 'del'],
+        ['superscript', 'subscript'],
+        ['link'],
+        ['image'],        
+        ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+        ['unorderedList', 'orderedList'],
+        ['horizontalRule'],
+        ['removeformat'],
+        ['fullscreen']
+    ]
 });
